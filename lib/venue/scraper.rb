6a3e496dc425285611f3c7624ec2d68c03447351
Rick Belson <belsonrj@aol.com>
2020-03-30 19:30:07 +0000

    fixed info

--- lib/venue/scraper.rb
+++ lib/venue/scraper.rb
@@ -20,28 +20,28 @@ class VenueTime::CLI::Scraper
     venue_1 = self.new
     venue_1.name = doc.search("body > div:nth-child(9) > div.content-with-sidebar > div.wysiwyg > p:nth-child(11) > a > strong").text
     venue_1.address = doc.search("body > div:nth-child(9) > div.content-with-sidebar > div.wysiwyg > p:nth-child(11) > em").text
-    venue_1.info = doc.search("/html/body/div[7]/div[2]/div[1]/p[6]/text()[1]").text
-    venue_1.sample_artists = doc.search("body > div:nth-child(9) > div.content-with-sidebar > div.wysiwyg > p:nth-child(11) > strong:nth-child(8)").text.strip
+    venue_1.info = doc.at("body > div:nth-child(9) > div.content-with-sidebar > div.wysiwyg > p:nth-child(11)").text
+
     
     venue_2 = self.new
     venue_2.name = doc.search("body > div:nth-child(9) > div.content-with-sidebar > div.wysiwyg > p:nth-child(21) > a > strong").text
     venue_2.address = doc.search("body > div:nth-child(9) > div.content-with-sidebar > div.wysiwyg > p:nth-child(21) > em").text
-    #venue_2.info = doc.search("")
+    venue_2.info = doc.search("body > div:nth-child(9) > div.content-with-sidebar > div.wysiwyg > p:nth-child(21)").text
         
     venue_3 = self.new
     venue_3.name = doc.search("body > div:nth-child(9) > div.content-with-sidebar > div.wysiwyg > p:nth-child(30) > a > strong").text
     venue_3.address = doc.search("body > div:nth-child(9) > div.content-with-sidebar > div.wysiwyg > p:nth-child(30) > em").text
-    #venue_3.info = doc.search("")
+    venue_3.info = doc.search("body > div:nth-child(9) > div.content-with-sidebar > div.wysiwyg > p:nth-child(30)").text
     
     venue_4 = self.new
     venue_4.name = doc.search("body > div:nth-child(9) > div.content-with-sidebar > div.wysiwyg > p:nth-child(25) > a > strong").text
     venue_4.address = doc.search("body > div:nth-child(9) > div.content-with-sidebar > div.wysiwyg > p:nth-child(25) > em").text
-    #venue_4.info = doc.search("")
+    venue_4.info = doc.search("body > div:nth-child(9) > div.content-with-sidebar > div.wysiwyg > p:nth-child(25)").text
     
     venue_5 = self.new
     venue_5.name = doc.search("body > div:nth-child(9) > div.content-with-sidebar > div.wysiwyg > p:nth-child(35) > a > strong").text
     venue_5.address = doc.search("body > div:nth-child(9) > div.content-with-sidebar > div.wysiwyg > p:nth-child(35) > em").text
-    #venue_5.info = doc.search("")
+    venue_5.info = doc.search("body > div:nth-child(9) > div.content-with-sidebar > div.wysiwyg > p:nth-child(35)").text
     
     @@all << venue_1
     @@all << venue_2
@@ -52,7 +52,7 @@ class VenueTime::CLI::Scraper
 
   def self.display_info(input)
     index = input.to_i - 1
-    puts "#{@@all[index].address}".green
+    puts "#{@@all[index].info}".yellow
   end
 
   
